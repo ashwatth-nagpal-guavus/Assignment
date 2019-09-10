8fedb27e6ad1f01139eb01f149fc7a01872d02a0
Ashwatth Nagpal <ashwatthnagpal@Ashwatth-Nagpal.local>
2019-09-10 22:53:06 +0530

    This commit compute tonnage and hits per url per minute

---
+++ src/main/scala/com/ashwatth/project/PerUrlTonnage.scala
@@ -0,0 +1,63 @@
+package com.ashwatth.project
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+object PerUrlTonnage {
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder
+      .master("local")
+      .appName("TopSubscribers")
+      .enableHiveSupport()
+      .getOrCreate()
+    import spark.implicits._
+
+    val smallSet = spark.read.orc("../../ashwatth/minhour/")
+
+    val validUrl =
+      smallSet
+        .filter($"httpUrl".isNotNull)
+        .filter(eachRow => checkUrl(eachRow.getString(11)))
+
+    val dfWithDomainName = validUrl
+      .select(
+        $"Hour",
+        $"transactionUplinkBytes",
+        $"transactionDownlinkBytes",
+        $"httpUrl",
+        $"min"
+      )
+      .map(eachRow => {
+        val http_url = eachRow.getString(3).split("/")
+        (
+          eachRow.getInt(0),
+          eachRow.getInt(1),
+          eachRow.getLong(2),
+          eachRow.getInt(4),
+          http_url(2)
+        )
+      })
+      .toDF(
+        "Hour",
+        "transactionUplinkBytes",
+        "transactionDownlinkBytes",
+        "min",
+        "domainName"
+      )
+
+    val tonnageAndHitsPerMinute =
+      dfWithDomainName
+        .groupBy("min", "domainName")
+        .agg(
+          (sum("transactionDownlinkBytes") + sum("transactionUplinkBytes"))
+            .as("Tonnage"),
+          count("domainName").as("Hits")
+        )
+        .orderBy(desc("Tonnage"))
+    tonnageAndHitsPerMinute.show(20, false)
+
+  }
+
+  def checkUrl(str: String): Boolean = {
+    str.split("/").size >= 3
+  }
+}
