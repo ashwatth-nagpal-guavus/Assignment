04e6e23cf881211b98bae32efba05e518fddb27b
Ashwatth Nagpal <ashwatthnagpal@Ashwatth-Nagpal.local>
2019-09-16 14:49:00 +0530

    change read statement for reading xml file

--- src/main/scala/com/ashwatth/project/Question5.scala
+++ src/main/scala/com/ashwatth/project/Question5.scala
@@ -2,7 +2,7 @@ package com.ashwatth.project
 
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
-
+import com.databricks.spark.xml._
 object Question5 {
   def main(args: Array[String]): Unit = {
     try {
@@ -12,15 +12,17 @@ object Question5 {
         .getOrCreate()
       import spark.implicits._
 
+
       val ggsnXMLDF = spark.read
         .option("rowTag", "ggsn")
-        .format("xml")
-        .load("/tmp/ashwatth/ggsn.xml")
+        .option("valueTag", "v")
+        .xml("/tmp/ashwatth/ggsn.xml")
 
       val ggsnFormattedDF = ggsnXMLDF
         .select($"_name", explode($"rule"))
         .select($"_name", $"col.condition._value")
         .toDF("name", "ggsnip")
+
 
       val edrHttpLogsDF = spark.table("ashwatth.edr_http_logs")
 
